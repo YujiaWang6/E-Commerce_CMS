4732e4df6af25c71a4c150e34f103f4171f7a72d
YujiaWang6 <wang.yujia111@gmail.com>
2023-08-15 00:53:49 -0400

    added api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +20,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/brands', function(){
+    $brands=Brand::orderBy('brandName')->get();
+    return $brands;
+});
+
+Route::get('/categories',function(){
+    $categories = Category::orderBy('categoryName')->get();
+    return $categories;
+});
+
+Route::get('/products',function(){
+    $products = Product::orderBy('created_at')->get();
+
+    foreach($products as $key => $product){
+        $products[$key]['brand']=Brand::where('id', $product['brand_id'])->first();
+        $products[$key]['category'] = Category::where('id', $product['category_id'])->first();
+
+    }
+
+    return $products;
 });
